ac6453692359c5945882aa4f6df47cd3efb9ccff
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-04 21:18:02 +0700

    finishing all routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const {
   addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
 } = require('./handler');
 
 const routes = [
@@ -8,7 +12,25 @@ const routes = [
     path: '/books',
     handler: addBookHandler,
   },
-
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
 ];
-
 module.exports = routes;
