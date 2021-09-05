214105f406de1e1e5bb2ef2e5f331a7d62ef72c5
ilhamsurya <ilhamsurya.m@gmail.com>
2021-09-05 09:23:44 +0700

    update bug

--- src/handler.js
+++ src/handler.js
@@ -21,7 +21,7 @@ const addBookHandler = (request, h) => {
   if (readPage > pageCount) {
     const response = h.response({
       status: 'fail',
-      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
     });
     response.code(400);
     return response;
